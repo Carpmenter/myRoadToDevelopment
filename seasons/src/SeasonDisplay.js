f15628ca21b69ba2feeea0f1b31bfe43a4da579c
Carpmenter <nickcarpenter1017@gmail.com>
2019-03-14 17:34:25 -0500

    React class components/ state | practice project

---
+++ seasons/src/SeasonDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class SeasonDisplay extends React.Component {
+    render() {
+        return (
+            <div>Season Display</div>
+        );
+    }
+ }
+
+ export default SeasonDisplay;
